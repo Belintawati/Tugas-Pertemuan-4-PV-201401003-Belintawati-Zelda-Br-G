b40d137b830ae12836a91e2fc7605d103fcee532
Belintawati <belintawatizld@gmail.com>
2022-03-02 16:14:05 +0700

    Tugaspertemuan4

---
+++ Tugas_Pertemuan_4/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 02/03/2022
+ * Time: 15.13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tugas_Pertemuan_4
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Label2Click(object sender, EventArgs e)
+		{	
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show ("Hello World"+Environment.NewLine+"Nama	: "+this.textBox1.Text+Environment.NewLine+"Nim	: "+this.textBox2.Text+Environment.NewLine+"Kelas	: "+this.textBox3.Text+Environment.NewLine+
+			"Matkul	: "+this.textBox4.Text);
+		}
+	}
+}
